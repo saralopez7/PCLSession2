4369e6a95b61f729e08498499ae35787598a932d
Sara Lopez <slopezalaguero@gmail.com>
2019-03-16 22:28:01 +0100

    Session 2 exercises

---
+++ Session2/PclYEx2.fs
@@ -0,0 +1,38 @@
+﻿module PclYEx2
+open System
+/// Exercise 2.1
+
+(*
+a = 5
+b = 15
+c = 20
+*)
+
+let a = 5
+let b = let a = 10 in a + 5
+let c = a + b
+
+/// Exercise 2.2
+
+let addNum1 x:int = x + 1
+        
+let addNum10 x = x + 10
+
+let addNum20 x:int = addNum10 (x) + 20
+
+/// Exercise 2.3
+let max2 (x:int) (y:int) = if x > y then x else y
+    
+let evenOrOdd x = if (x % 2) = 0 then printfn "Even number" else  printfn "Odd number"
+
+let addXY (x:int) (y:int) = 
+    printfn "%i %i" x y 
+    x + y
+
+let rec addNumbersRec j k = 
+    if k = 0 then      
+        j 
+    else
+        addNumbersRec j (k-1) + addNum10 0
+    
+let addNumbers j k = j + 10*k
---
+++ Session2/Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+
+
+open System
+
+(*
+multi-line comments
+*)
+let time = DateTime.Today.ToString("yyyy.MM.dd hh:mm:ss")
+printfn "%s" time
+
+// a simple function with integers
+let function1 x = 2 + 4 + x * 4
+
+let rec vfactorial x:int =  
+    if x <= 0 then
+        1
+    else 
+        x * vfactorial (x - 1)
+
+let data = (12, "Valentine", 134)
+let funSwap (a, b) = (b, a)
+// Swapping a point
+let pointA = (12, 134)
+
+let pointB = funSwap pointA
+
+let oddNums = [1; 3; 5; 7; 9]
+
+let consist = 1 :: [2; 3]
+
+let enVowels = ['a'; 'e'; 'i'; 'o'; 'u']
+
+let squareOfFirst10Integers = [for i in 1..10 -> i * i]
+   
